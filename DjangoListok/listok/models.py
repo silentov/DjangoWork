5180647ccddbab25bc121589f0631903eb5331a7
silentov <kirill-filatof2013@yandex.ru>
2021-10-19 14:34:19 +0300

    добавил шаблоны

--- DjangoListok/listok/models.py
+++ DjangoListok/listok/models.py
@@ -54,19 +54,19 @@ class User(models.Model):
     gender = models.CharField(max_length=1, choices=gender_choise, verbose_name='Пол')
     number = models.CharField(max_length=30, verbose_name='Телефон')
     number2 = models.CharField(max_length=30, verbose_name='Телефон (альтернативный)', blank=True)
-    birthday_date = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name='Год рождения')
-    adres = models.CharField(max_length=255, verbose_name='Адрес')
+    birthday_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Дата рождения')
+    adres = models.CharField(max_length=255, verbose_name='Адрес', blank=True)
     code = models.CharField(max_length=255, verbose_name='Штрих-код')
     family_field = models.CharField(max_length=255, verbose_name='Карточка родителя', blank=True)
     FIO_family = models.CharField(max_length=255, verbose_name='ФИО роидтеля', blank=True)
-    number_family = models.CharField(max_length=255, verbose_name='Телефон родителя')
+    number_family = models.CharField(max_length=255, verbose_name='Телефон родителя', blank=True)
     email = models.CharField(max_length=255, verbose_name='Email', blank=True)
-    status = models.ForeignKey(Status, verbose_name='Статус', on_delete=models.PROTECT)
+    status = models.ForeignKey(Status, verbose_name='Статус', on_delete=models.PROTECT, blank=True)
     ab_status = models.CharField(max_length=255, verbose_name='Статус абонемента')      # придумать метод и формат поля
     blacklist = models.CharField(max_length=1, verbose_name='Черный список', choices=blacklist_choise)
-    source = models.ForeignKey('Source', verbose_name='Источник', on_delete=models.PROTECT)
+    source = models.ForeignKey('Source', verbose_name='Источник', on_delete=models.PROTECT, blank=True)
     class_format = models.ForeignKey('ClassFormat', verbose_name='Формат занятий', on_delete=models.PROTECT)
-    personal_disc = models.PositiveSmallIntegerField(blank=True)
+    personal_disc = models.PositiveSmallIntegerField(verbose_name='Персональная скидка', blank=True)
     Vk_id = models.CharField(max_length=255, verbose_name='Вконтакте ID', blank=True)
     Fb_id = models.CharField(max_length=255, verbose_name='Facebook ID', blank=True)
     Ig_id = models.CharField(max_length=255, verbose_name='Instagramm ID', blank=True)
@@ -76,23 +76,9 @@ class User(models.Model):
     notes_unvi = models.TextField(max_length=1000, verbose_name='Заметки', blank=True)
     notes_visible = models.TextField(max_length=500, verbose_name='Заметки (видимые)', blank=True)
     passport = models.PositiveIntegerField(verbose_name='Номер и серия паспорта')
-    passport_date = models.DateTimeField(auto_now=False, auto_now_add=False, verbose_name='Дата выдачи паспорта')
+    passport_date = models.DateField(auto_now=False, auto_now_add=False, verbose_name='Дата выдачи паспорта')
     passport_org = models.CharField(max_length=255, verbose_name='Кем выдан')
     org_code = models.CharField(max_length=20, verbose_name='Дата выдачи')
 
-
-# нельзя создавать ссылку на поля
-
-#class Abonements(models.Model):
-
-#    class Meta:
-#       verbose_name = "Абонементы"
-
-#    Fio = models.ForeignKey('User.FIO', verbose_name='ФИО', on_delete=models.PROTECT)
-#    birthday = models.ForeignKey('User.birthday_date', verbose_name='Дата рождения', on_delete=models.PROTECT)
-#   phone_number = models.ForeignKey('User.number', verbose_name='Номер телефона', on_delete=models.PROTECT)
-#    abonement_status = models.ForeignKey('User.ab_status', verbose_name='Статус абонемента', on_delete=models.PROTECT)
-
-
-#    def __str__(self):
-#        return self.abonement_status
+    def __str__(self):
+        return self.ab_status
